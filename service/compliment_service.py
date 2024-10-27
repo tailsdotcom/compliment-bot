1dd66b8881b703ab45ed0b01713ad20a3886edaf
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-10-27 20:31:32 +0000

    Strip out all the insult code

--- service/compliment_service.py
+++ service/compliment_service.py
@@ -1,29 +1,10 @@
-import random
-
 from service.messages import (
     BLOCK_DIVIDER,
-    COMPLIMENT_MSG,
-    CONFIRMATION_MSG,
-    COMPLIMENT_HEADER,
     get_compliment_block,
-    ACCESSORY_COMPLIMENT,
-    ACCESSORY_INSULT,
 )
-
-COMPLIMENT_CHANCE: float = 1
 
 
 class ComplimentService:
-    def __init__(self):
-        self.mode = "insult" if random.random() > COMPLIMENT_CHANCE else "compliment"
-
-    def reroll_compliment(self):
-        self.mode = "insult" if random.random() > COMPLIMENT_CHANCE else "compliment"
-
-    @property
-    def insult(self):
-        return self.mode == "insult"
-
     def _get_msg_context(self, recipients, sender=None, message=None):
         names = [user["real_name"] for user in recipients]
         if len(recipients) > 1:
@@ -45,7 +26,7 @@ class ComplimentService:
         return context
 
     def get_confirmation_msg(self, sender, recipients) -> str:
-        msg = random.choice(CONFIRMATION_MSG[self.mode])
+        msg = "Compliment sent to {recipient_names}!"
 
         context = self._get_msg_context(recipients, sender)
 
@@ -54,21 +35,12 @@ class ComplimentService:
     def get_compliment(self, sender, recipients, message: str) -> str:
         context = self._get_msg_context(recipients, sender, message)
 
-        default_msg = random.choice(COMPLIMENT_MSG[self.mode]).format(**context)
+        default_msg = message
 
-        block_header = get_compliment_block(random.choice(COMPLIMENT_HEADER[self.mode]))
+        block_header = get_compliment_block("Someone sent you a compliment!")
 
         body_block = get_compliment_block(default_msg)
-        if self.insult:
-            body_block.update(ACCESSORY_INSULT)  #  else ACCESSORY_COMPLIMENT
 
         blocks = [block_header, BLOCK_DIVIDER.copy(), body_block]
-
-        if self.insult:
-            blocks.append(BLOCK_DIVIDER.copy())
-
-            blocks.append(get_compliment_block("This is what they _really_ said..."))
-
-            blocks.append(get_compliment_block(f">{message}"))
 
         return default_msg, blocks
